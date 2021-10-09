b77e4eee552a28dceb975d5d1b26986db017b0e4
O-Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-10-09 13:18:56 +0200

    Task 3: create parameter and set it value in java app

--- jasperreportspringboot/src/main/java/com/jasperreport/JasperreportspringbootApplication.java
+++ jasperreportspringboot/src/main/java/com/jasperreport/JasperreportspringbootApplication.java
@@ -3,11 +3,24 @@ package com.jasperreport;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
 @SpringBootApplication
 public class JasperreportspringbootApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JasperreportspringbootApplication.class, args);
+
+        try{
+            ClassLoader classLoader = JasperreportspringbootApplication.class.getClassLoader();
+            File file = new File(classLoader.getResource("first_report.jrxml").getFile());
+            Map<String,Object> parameters = new HashMap<>();
+            parameters.put("studentName","Abdelaaziz");
+        }catch (Exception ex){
+            System.out.println(ex);
+        }
     }
 
 }
