2d0937d195647e1bdfb3320ba9e4018199aba7c4
O-Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-10-09 12:01:09 +0200

    Task 1: first commit & upload project

---
+++ jasperreportspringboot/src/main/java/com/jasperreport/JasperreportspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.jasperreport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JasperreportspringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JasperreportspringbootApplication.class, args);
+    }
+
+}
