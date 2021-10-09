f2bc761adbfed6dd3742a61dc76a9930a2d5bacf
O-Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-10-09 16:25:00 +0200

    Task 4: setting up field for table data

---
+++ jasperreportspringboot/src/main/java/com/jasperreport/Employee.java
@@ -0,0 +1,64 @@
+package com.jasperreport;
+
+/**
+ * @Created 09/10/2021 - 13:20
+ * @Package com.jasperreport
+ * @Project jasperreportspringboot
+ * @User LegendDZ
+ * @Author Abdelaaziz Ouakala
+ **/
+public class Employee {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String street;
+    private String city;
+
+    public Employee(Long id, String firstName, String lastName, String street, String city) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.street = street;
+        this.city = city;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
--- jasperreportspringboot/src/main/java/com/jasperreport/JasperreportspringbootApplication.java
+++ jasperreportspringboot/src/main/java/com/jasperreport/JasperreportspringbootApplication.java
@@ -1,10 +1,13 @@
 package com.jasperreport;
 
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @SpringBootApplication
@@ -18,6 +21,15 @@ public class JasperreportspringbootApplication {
             File file = new File(classLoader.getResource("first_report.jrxml").getFile());
             Map<String,Object> parameters = new HashMap<>();
             parameters.put("studentName","Abdelaaziz");
+
+            Employee employee1=new Employee(1L, "employee1","emloyee1","street1","city1");
+            Employee employee2=new Employee(2L, "employee2","emloyee2","street2","city2");
+
+            List<Employee> stringList=new ArrayList<>();
+            stringList.add(employee1);
+            stringList.add(employee2);
+            
+            JRBeanCollectionDataSource collectionDataSource=new JRBeanCollectionDataSource(stringList);
         }catch (Exception ex){
             System.out.println(ex);
         }
